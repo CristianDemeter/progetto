64615691e5ba8bcf7faeb72592aee3bb2b7577dd
CristianDemeter <demeter.cri@gmail.com>
2022-10-31 13:33:25 +0000

    aggiunto composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'contact/index',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '25675b384d9c8d2e61edbd1b4e2ccfa4312b0809',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'contact/index' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '25675b384d9c8d2e61edbd1b4e2ccfa4312b0809',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v226',
+            'version' => '226.0.0.0',
+            'reference' => '9d94153147d2fd68f34c82cbbf9b6f02eb1e01b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
